efc4e6e02863edae7d9c2eabec7135c5f4474696
jinhyun <jinhyun0504@hanmail.net>
2019-07-29 20:51:48 +0900

    퍼블 css, 디자인 적용

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,54 +1,52 @@
 <template>
-  <article class="contents index">
-    <div class="login_contn">
+  <div class="contents index">
+    <article class="login_contn">
       <section class="title style-2">
-        <h2><i class="ico-bar" />LOGIN</h2>
+        <h2><i class="ico-bar" />META  PORTAL  WORLD</h2>
       </section>
-      <div class="login_form">
-        <section class="login form_area border_group">
-          <div class="row_contain type-1">
-            <div class="column on w-4">
-              <label class="column_label">ID</label>
-              <input
-                v-model="user_id"
-                type="text"
-                value=""
-              >
-            </div>
+      <section class="login form_area border_group">
+        <div class="row_contain type-1">
+          <div class="column on w-4">
+            <label class="column_label">ID</label>
+            <input
+              v-model="user_id"
+              type="text"
+              value=""
+            >
           </div>
-          <div class="row_contain type-1">
-            <div class="column w-4">
-              <label class="column_label">Password</label>
-              <input
-                v-model="user_pw"
-                type="password"
-                value="SKCC"
-              >
-            </div>
-          </div>
-        </section>
-        <section class="btm_button_area">
-          <button
-            type="button"
-            class="default_button on"
-            @click="login()"
-          >
-            LOGIN
-          </button>
-          <button
-            type="button"
-            class="default_button"
-          >
-            SIGN UP
-          </button>
-        </section>
-        <div class="help">
-          <em class="notice">forgot password</em>
-          <i class="ico-help" />
         </div>
-      </div>
-    </div>
-  </article>
+        <div class="row_contain type-1">
+          <div class="column w-4">
+            <label class="column_label">Password</label>
+            <input
+              v-model="user_pw"
+              type="password"
+              value="SKCC"
+            >
+          </div>
+        </div>
+      </section>
+      <section class="btm_button_area">
+        <button
+          type="button"
+          class="default_button on"
+          @click="login()"
+        >
+          LOGIN
+        </button>
+        <button
+          type="button"
+          class="default_button"
+        >
+          SIGN UP
+        </button>
+      </section>
+      <section class="help">
+        <em class="notice">forgot password</em>
+        <i class="ico-help" />
+      </section>
+    </article>
+  </div>
 </template>
 
 <script>
