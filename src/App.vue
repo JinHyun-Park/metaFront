6df06d7b625742f07f0453fde50b32256b67f7f1
soonockyun <felyi27@sk.com>
2019-09-06 13:54:40 +0900

    Recover css and .vue fort popup

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
-    <div class="confirm">
-      <comm-alert v-if="true" />
-    </div>
+    <comm-alert v-if="true" />
     <div v-if="loginPageOn">
       <router-view />
     </div>
