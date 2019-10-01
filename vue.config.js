882315c2a0592b77e52c1d5e409359de292e8c4a
P064066 <P064066@SKTP064066PN002>
2019-10-01 09:26:17 +0900

    was서버 IP 변경으로 vueconfig 변경

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  transpileDependencies: ['vuex-persist'],
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://35.194.166.192:8081',
-        // target: 'http://localhost:8081',
-        changOrigin: true,
-        pathRewrite: {
-          '^/api': '',
+    transpileDependencies: ['vuex-persist'],
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://104.155.212.215:8081',
+                //target: 'http://localhost:8081',
+                changOrigin: true,
+                pathRewrite: {
+                    '^/api': '',
+                },
+            },
         },
-      },
     },
-  },
 };
