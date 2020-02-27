b0226183732c35a0cc4272e338c6a27da40d39de
jinhyun <jinhyun0504@hanmail.net>
2020-02-27 09:29:55 +0900

    run serve에 지장주는 요소 제거

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,20 +6,8 @@ module.exports = {
   extends: [
     'plugin:vue/recommended', // essential에서 recommended로 변경
     '@vue/airbnb',
-    '@vue/prettier',
-    'prettier',
   ],
   rules: {
-    "prettier/prettier": ['error', {
-      singleQuote: true,
-      semi: true,
-      userTabs: true,
-      tabWidth: 2,
-      trailingComma: 'all',
-      printWidth: 80,
-      bracketSpacing: true,
-      arrowParens: 'avoid',
-    }],
     'linebreak-style': 0,
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
