3c34b2f4fd212a2dabea8becf118ef479462e487
장호은(JANG HO EUN)/ICT혁신팀/SK <09348@skcc.com>
2020-03-18 16:58:54 +0900

    20200318_mcgChnlv0.3

--- src/api/mcgApi.js
+++ src/api/mcgApi.js
@@ -2,11 +2,49 @@
 const preUrl = '/api/mcg';
 
 function fetchGetMcgChnlList(param) {
-    return axios.get(`${preUrl}/chnl`, param);
+  return axios.get(`${preUrl}/chnl`, param);
 }
 
+function fetchGetMcgServerList(param) {
+  return axios.get(`${preUrl}/chnl/server`, param);
+}
+
+function fetchGetMcgChrgrList(param) {
+  return axios.get(`${preUrl}/chnl/chrgr`, param);
+}
+
+function fetchPostMcgChnlList(param) {
+  return axios.post(`${preUrl}/chnl/post`, param);
+}
+
+function fetchPostMcgServerList(param) {
+  return axios.post(`${preUrl}/chnl/server/post`, param);
+}
+
+function fetchPostMcgChrgrList(param) {
+  return axios.post(`${preUrl}/chnl/chrgr/post`, param);
+}
+
+function fetchPutMcgChnlList(param) {
+  return axios.put(`${preUrl}/chnl/put`, param);
+}
+
+function fetchPutMcgServerList(param) {
+  return axios.put(`${preUrl}/chnl/server/put`, param);
+}
+
+function fetchPutMcgChrgrList(param) {
+  return axios.put(`${preUrl}/chnl/chrgr/put`, param);
+}
 
 export {
-    // eslint-disable-next-line import/prefer-default-export
-    fetchGetMcgChnlList,
+  fetchGetMcgChnlList,
+  fetchGetMcgServerList,
+  fetchGetMcgChrgrList,
+  fetchPostMcgChnlList,
+  fetchPostMcgServerList,
+  fetchPostMcgChrgrList,
+  fetchPutMcgChnlList,
+  fetchPutMcgServerList,
+  fetchPutMcgChrgrList,
 };
