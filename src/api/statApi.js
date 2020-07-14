290f83905264d747b2bf810ef6d9d7d3a2a194d5
june <june@218.38.137.27>
2020-07-14 15:28:25 +0900

    통계 API 추가

--- src/api/statApi.js
+++ src/api/statApi.js
@@ -2,17 +2,34 @@
 const preUrl = '/api/statistic';
 
 function fetchGetStatEaiDailyTrms(param) {
-  return axios.get(`${preUrl}/daily/eai`, param);
+    return axios.get(`${preUrl}/daily/eai`, param);
 }
+
 function fetchGetStatEigwDailyTrms(param) {
-  return axios.get(`${preUrl}/daily/eigw`, param);
+    return axios.get(`${preUrl}/daily/eigw`, param);
 }
+
 function fetchGetStatMcgDailyTrms(param) {
-  return axios.get(`${preUrl}/daily/mcg`, param);
+    return axios.get(`${preUrl}/daily/mcg`, param);
+}
+
+function fetchGetStatEaiHourlyTrms(param) {
+    return axios.get(`${preUrl}/hourly/eai`, param);
+}
+
+function fetchGetStatEigwHourlyTrms(param) {
+    return axios.get(`${preUrl}/hourly/eigw`, param);
+}
+
+function fetchGetStatMcgHourlyTrms(param) {
+    return axios.get(`${preUrl}/hourly/mcg`, param);
 }
 
 export {
-  fetchGetStatEaiDailyTrms,
-  fetchGetStatEigwDailyTrms,
-  fetchGetStatMcgDailyTrms,
+    fetchGetStatEaiDailyTrms,
+    fetchGetStatEigwDailyTrms,
+    fetchGetStatMcgDailyTrms,
+    fetchGetStatEaiHourlyTrms,
+    fetchGetStatEigwHourlyTrms,
+    fetchGetStatMcgHourlyTrms,
 };
