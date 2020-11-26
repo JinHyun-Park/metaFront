697e31a65416a46e1a1a2e68f6dc6c1e992e0216
강태훈님/ServiceGW <P157590@sktelecom.com>
2020-11-26 13:31:25 +0900

    운영기 반영목록 화면 프로세스 추가, 수정, 삭제 기능 추가

--- src/views/admin/ifMgmt/eigw/regOnline.vue
+++ src/views/admin/ifMgmt/eigw/regOnline.vue
@@ -24,6 +24,12 @@
       @closePop="turOffSvrPopChrgr"
       @addData="addDataChrgr"
     />
+    <EigwServerListPopup
+      v-if="svrOn"
+      v-bind="props"
+      @closePop="turOffSvrPop"
+      @addData="setSvr"
+    />
     <section class="title style-1">
       <h2>
         <div>
@@ -262,7 +268,7 @@
           <button
             type="button"
             class="default_button"
-            @click="setOnlineInfoFromReqOnlineInfo(), setOnlineChrgrFromReqChrgrList()"
+            @click="setOnlineInfoFromReqOnlineInfo(), setOnlineChrgrFromReqChrgrList(), setProcInfoFromReqOnlineInfo()"
           >
             신청 정보 세팅
           </button>
@@ -525,6 +531,291 @@
         </div>
       </div>
     </section>
+    <section class="form_area border_group">
+      <h5 class="s_tit type-2">
+        프로세스 정보
+      </h5>
+      <div class="table_colgroup">
+        <div class="table_grid">
+          <div class="table_head">
+            <ul>
+              <li class="th_cell">
+                프로그램명
+              </li>
+              <li class="th_cell">
+                설정파일
+              </li>
+              <li class="th_cell">
+                개발IP(NAT)
+              </li>
+              <li class="th_cell">
+                개발 PORT
+              </li>
+              <li class="th_cell">
+                운영IP(NAT)
+              </li>
+              <li class="th_cell">
+                운영 PORT
+              </li>
+              <li class="th_cell">
+                삭제
+              </li>
+            </ul>
+          </div>
+          <div
+            v-if="onlineMetaNum === '' || onlineMetaNum === undefined || onlineMetaNum === null"
+            class="table_body"
+          >
+            <ul class="table_row">
+              <li class="td_cell">
+                상단의 인터페이스 신규신청을 먼저 해주시기바랍니다.
+              </li>
+            </ul>
+          </div>
+          <div
+            v-else-if="procList.length === 0"
+            class="table_body"
+          >
+            <ul class="table_row">
+              <li class="td_cell">
+                프로세스를 생성해주세요.
+              </li>
+            </ul>
+          </div>
+          <div
+            v-else
+            class="table_body"
+          >
+            <ul
+              v-for="(row, index) in procList"
+              :key="row.procNum"
+              class="table_row w-auto"
+              @click="selectProcInfo(index)"
+            >
+              <li class="td_cell">
+                {{ row.pgmId }}
+              </li>
+              <li class="td_cell">
+                {{ row.confFile }}
+              </li>
+              <li class="td_cell">
+                {{ row.dvpSvrRealIp }}<br>({{ row.dvpSvrNatIp }})
+              </li>
+              <li class="td_cell">
+                {{ row.dvpSvrPort }}
+              </li>
+              <li class="td_cell">
+                {{ row.prodSvrRealIp }}<br>({{ row.prodSvrNatIp }})
+              </li>
+              <li class="td_cell">
+                {{ row.prodSvrPort }}
+              </li>
+              <li class="td_cell">
+                <i
+                  class="ico-del"
+                  @click="deleteProcInfo(row.procNum, index)"
+                />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="form_area border_group">
+      <h5 class="s_tit type-2">
+        프로세스 세부정보
+        <div class="right_button_area">
+          <button
+            type="button"
+            class="default_button"
+            @click="addProcInfo()"
+          >
+            신규신청
+          </button>
+          <button
+            type="button"
+            class="default_button"
+            @click="updateProcInfo()"
+          >
+            수정
+          </button>
+        </div>
+      </h5>
+      <div class="row_contain type-2">
+        <div class="column on w-1">
+          <label class="column_label">프로그램</label>
+          <input
+            v-model="procInfo.pgmId"
+            type="text"
+          >
+        </div>
+        <div class="column w-2">
+          <label class="column_label">프로세스설명</label>
+          <input
+            v-model="procInfo.procDesc"
+            type="text"
+          >
+        </div>
+        <div class="column w-1">
+          <label class="column_label">프로그램유형</label>
+          <select v-model="procInfo.pgmTyp">
+            <option value="CLIENT">
+              클라이언트
+            </option>
+            <option value="SERVER">
+              서버
+            </option>
+          </select>
+          <span class="select" />
+        </div>
+      </div>
+      <div class="row_contain">
+        <div class="column w-2">
+          <label class="column_label">설정파일</label>
+          <input
+            v-model="procInfo.confFile"
+            type="text"
+          >
+        </div>
+        <div class="column w-2">
+          <label class="column_label">세션수</label>
+          <input
+            v-model="procInfo.sesnCnt"
+            type="text"
+          >
+        </div>
+        <div class="column w-2">
+          <label class="column_label">소켓타임아웃</label>
+          <input
+            v-model="procInfo.socketTimeout"
+            type="text"
+          >
+        </div>
+        <div class="column w-2">
+          <label class="column_label">MQ타임아웃</label>
+          <input
+            v-model="procInfo.mqTimeout"
+            type="text"
+          >
+        </div>
+      </div>
+      <div class="row_contain">
+        <div class="column w-3">
+          <label class="column_label">연결유형</label>
+          <select v-model="procInfo.linkTyp">
+            <option value="CONN">
+              연결형
+            </option>
+            <option value="DISCONN">
+              비연결형
+            </option>
+          </select>
+          <span class="select" />
+        </div>
+        <div class="column w-3">
+          <label class="column_label">Log파일명</label>
+          <input
+            v-model="procInfo.logFileNm"
+            type="text"
+          >
+        </div>
+        <div class="column w-3">
+          <label class="column_label">Log파일경로</label>
+          <input
+            v-model="procInfo.logPath"
+            type="text"
+          >
+        </div>
+        <div class="column w-3">
+          <label class="column_label">사용유무</label>
+          <select v-model="procInfo.useYn">
+            <option value="Y">
+              사용
+            </option>
+            <option value="N">
+              미사용
+            </option>
+          </select>
+          <span class="select" />
+        </div>
+        <div class="column w-2">
+          <input
+            v-model="procInfo.procNum"
+            type="hidden"
+          >
+        </div>
+      </div>
+      <div class="row_contain">
+        <div class="column w-2">
+          <label class="column_label">개발기 REAL Ip</label>
+          <input
+            v-model="procInfo.dvpSvrRealIp"
+            type="text"
+            class="add_text"
+            @click="turnOnSvrPop(1)"
+          >
+        </div>
+        <div class="column w-2">
+          <label class="column_label">개발기 NAT IP</label>
+          <input
+            v-model="procInfo.dvpSvrNatIp"
+            type="text"
+            class="add_text"
+            @click="turnOnSvrPop(1)"
+          >
+        </div>
+        <div class="column w-2">
+          <label class="column_label">개발기 Port</label>
+          <input
+            v-model="procInfo.dvpSvrPort"
+            type="number"
+            class="add_text"
+          >
+        </div>
+        <div class="column w-2">
+          <input
+            v-model="procInfo.dvpSvrNum"
+            type="hidden"
+            class="add_text"
+          >
+        </div>
+      </div>
+      <div class="row_contain">
+        <div class="column w-2">
+          <label class="column_label">운영기 REAL Ip</label>
+          <input
+            v-model="procInfo.prodSvrRealIp"
+            type="text"
+            class="add_text"
+            @click="turnOnSvrPop(2)"
+          >
+        </div>
+        <div class="column w-2">
+          <label class="column_label">운영기 NAT IP</label>
+          <input
+            v-model="procInfo.prodSvrNatIp"
+            type="text"
+            class="add_text"
+            @click="turnOnSvrPop(2)"
+          >
+        </div>
+        <div class="column w-2">
+          <label class="column_label">운영기 Port</label>
+          <input
+            v-model="procInfo.prodSvrPort"
+            type="number"
+            class="add_text"
+          >
+        </div>
+        <div class="column w-2">
+          <input
+            v-model="procInfo.prodSvrNum"
+            type="hidden"
+            class="add_text"
+          >
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 <script>
@@ -534,7 +825,7 @@ import InstListPopup from '@/components/popup/bizcomm/InstListPopup.vue';
 import EaiListPopup from '@/components/popup/meta/eigw/EaiListPopup.vue';
 import ChrgrListPopup from '@/components/popup/bizcomm/ChrgrListPopup.vue';
 import EigwChrgrListPopup from '@/components/popup/meta/eigw/EigwChrgrListPopup.vue';
-// import EigwServerListPopup from '@/components/popup/meta/eigw/EigwServerListPopup.vue';
+import EigwServerListPopup from '@/components/popup/meta/eigw/EigwServerListPopup.vue';
 
 export default {
   components: {
@@ -542,6 +833,7 @@ export default {
     EaiListPopup,
     ChrgrListPopup,
     EigwChrgrListPopup,
+    EigwServerListPopup,
   },
   data() {
     return {
@@ -600,6 +892,32 @@ export default {
       ],
       svrOnEigwChrgr: false,
       propsEigwChrgr: { // 조회 시 parameter에 사용자 정보를 담아주려면 여기를 통해 넘겨주세요.
+        message: '', // 사용방법 예시 데이터
+      },
+      procList: [],
+      onlineMetaNum: '',
+      procInfo: {
+        procNum: 0,
+        pgmId: '',
+        procDesc: '',
+        useYn: '',
+        sesnCnt: '',
+        logPath: '',
+        logFileNm: '',
+        socketTimeout: '',
+        mqTimeout: '',
+        pgmTyp: '',
+        linkTyp: '',
+        confFile: '',
+        dvpSvrNum: '',
+        dvpSvrIp: '',
+        prodSvrNum: '',
+        prodSvrIp: '',
+        dvpSvrPort: '',
+        prodSvrPort: '',
+      },
+      svrOn: false,
+      props: { // 조회 시 parameter에 사용자 정보를 담아주려면 여기를 통해 넘겨주세요.
         message: '', // 사용방법 예시 데이터
       },
     };
@@ -676,8 +994,13 @@ export default {
       this.onlineInfo.instCd = this.reqOnlineInfo.instCd;
       this.onlineInfo.instNm = this.reqOnlineInfo.instNm;
     },
-    addOnlineInfo() {
-      console.log('addOnlineInfo()');
+    setProcInfoFromReqOnlineInfo() {
+      this.procInfo.pgmTyp = this.reqOnlineInfo.pgmTyp;
+      this.procInfo.linkTyp = this.reqOnlineInfo.linkTyp;
+      this.procInfo.dvpSvrPort = this.reqOnlineInfo.devPort;
+      this.procInfo.prodSvrPort = this.reqOnlineInfo.prodPort;
+      console.log(this.procInfo.pgmTyp);
+      console.log(this.reqOnlineInfo.pgmTyp);
     },
     searchInChrgr(row) {
       this.row = row;
@@ -822,6 +1145,219 @@ export default {
       if (this.outChrgrlist.length === 0) {
         this.initOutChrgrList();
       }
+    },
+    addOnlineInfo() {
+      if (this.onlineInfo.eaiIfId === '') {
+        this.$gf.alertOn('I/F ID를 입력해주세요.');
+      }
+      if (this.onlineInfo.instCd === '') {
+        this.$gf.alertOn('대외기관을 입력해주세요.');
+      }
+      for (let i = 0; i < this.inChrgrList.length; i++) {
+        if (this.inChrgrList[i].userId === '' || this.outChrgrList[i].userId === undefined) {
+          this.$gf.alertOn('담당자 정보를 입력하세요');
+          return;
+        }
+      }
+      for (let i = 0; i < this.outChrgrList.length; i++) {
+        if (this.outChrgrList[i].chrgrTyp === '' || this.outChrgrList[i].chrgrTyp === undefined) {
+          this.$gf.alertOn('대외기관 담당자 구분을 선택하세요');
+          return;
+        } if (this.outChrgrList[i].chrgrTyp === 'new') {
+          if (this.outChrgrList[i].ofcLvlCd === '' || this.outChrgrList[i].instNm === ''
+          || this.outChrgrList[i].instCd === '' || this.outChrgrList[i].hanNm === ''
+          || this.outChrgrList[i].instCd === undefined
+          || this.outChrgrList[i].hanNm === undefined
+          || this.outChrgrList[i].ofcLvlCd === undefined
+          || this.outChrgrList[i].instNm === undefined
+          || this.outChrgrList[i].mblPhonNum === undefined
+          || this.outChrgrList[i].emailAddr === undefined
+          || this.outChrgrList[i].mblPhonNum === '' || this.outChrgrList[i].emailAddr === '') {
+            this.$gf.alertOn('대외기관 담당자 정보를 입력하세요');
+            return;
+          }
+        } else if (this.outChrgrList[i].userId === undefined || this.outChrgrList[i].userId === '') {
+          this.$gf.alertOn('대외기관 담당자 정보를 입력하세요');
+          return;
+        }
+      }
+      this.saveInfo = {
+        onlineInfo: this.onlineInfo,
+        inChrgrList: this.inChrgrList,
+        outChrgrList: this.outChrgrList,
+      };
+      eigwApi.fetchPostEigwOnlineInfo(this.saveInfo)
+        .then((res) => {
+          console.log(res);
+          this.$gf.alertOn('인터페이스가 등록되었습니다.');
+          this.onlineMetaNum = res.data.rstData.onlineMetanum;
+          this.onlineInfo.onlineMetaNum = this.onlineMetaNum;
+          console.log(this.onlineMetaNum);
+        })
+        .catch((ex) => {
+          console.log(`metainfo save error occur!! : ${ex}`);
+        });
+    },
+    searchProcList(onlineMetaNum) {
+      eigwApi.fetchGetEigwProcList({
+        params: {
+          onlineMetaNum,
+        },
+      })
+        .then((res) => {
+          console.log(res);
+          if (res.data.rstCd === 'S') {
+            this.procList = res.data.rstData.procList;
+          } else {
+            this.$gf.alertOn('failed');
+          }
+        })
+        .catch((ex) => {
+          console.log(`error occur!! : ${ex}`);
+        });
+    },
+    initProcInfo() {
+      this.procInfo.procNum = 0;
+      this.procInfo.pgmId = '';
+      this.procInfo.procDesc = '';
+      this.procInfo.useYn = '';
+      this.procInfo.sesnCnt = '';
+      this.procInfo.logPath = '';
+      this.procInfo.logFileNm = '';
+      this.procInfo.socketTimeout = '';
+      this.procInfo.mqTimeout = '';
+      this.procInfo.pgmTyp = '';
+      this.procInfo.linkTyp = '';
+      this.procInfo.confFile = '';
+      this.procInfo.dvpSvrNum = '';
+      this.procInfo.dvpSvrIp = '';
+      this.procInfo.dvpSvrRealIp = '';
+      this.procInfo.dvpSvrNatIp = '';
+      this.procInfo.prodSvrNum = '';
+      this.procInfo.prodSvrIp = '';
+      this.procInfo.prodSvrRealIp = '';
+      this.procInfo.prodSvrNatIp = '';
+      this.procInfo.dvpSvrPort = '';
+      this.procInfo.prodSvrPort = '';
+    },
+    addProcInfo() {
+      if (this.onlineInfo === '' || this.onlineInfo.onlineMetaNum === 0) {
+        this.$gf.alertOn('I/F를 선택해주세요.');
+        return;
+      }
+      if (this.procInfo.pgmId === '' || this.procInfo.pgmId === undefined) {
+        this.$gf.alertOn('프로그램 정보를 입력해주세요.');
+        return;
+      }
+      eigwApi.fetchPostEigwProcInfo({
+        onlineMetaNum: this.onlineInfo.onlineMetaNum,
+        pgmId: this.procInfo.pgmId,
+        confFile: this.procInfo.confFile,
+        procDesc: this.procInfo.procDesc,
+        useYn: this.procInfo.useYn,
+        sesnCnt: this.procInfo.sesnCnt,
+        logPath: this.procInfo.logPath,
+        logFileNm: this.procInfo.logFileNm,
+        socketTimeout: this.procInfo.socketTimeout,
+        mqTimeout: this.procInfo.mqTimeout,
+        pgmTyp: this.procInfo.pgmTyp,
+        linkTyp: this.procInfo.linkTyp,
+        dvpSvrNum: this.procInfo.dvpSvrNum,
+        dvpSvrPort: this.procInfo.dvpSvrPort,
+        prodSvrNum: this.procInfo.prodSvrNum,
+        prodSvrPort: this.procInfo.prodSvrPort,
+      })
+        .then((res) => {
+          console.log(res);
+          if (res.data.rstCd === 'S') {
+            this.procInfo.procNum = res.data.rstData.procNum;
+            this.searchProcList(this.onlineInfo.onlineMetaNum);
+            this.initProcInfo();
+            this.$gf.alertOn('프로세스가 추가되었습니다.');
+          } else {
+            this.$gf.alertOn('failed');
+          }
+        })
+        .catch((ex) => {
+          console.log(`error occur!! : ${ex}`);
+        });
+    },
+    updateProcInfo() {
+      eigwApi.fetchPutEigwProcInfo({
+        procNum: this.procInfo.procNum,
+        pgmId: this.procInfo.pgmId,
+        confFile: this.procInfo.confFile,
+        procDesc: this.procInfo.procDesc,
+        useYn: this.procInfo.useYn,
+        sesnCnt: this.procInfo.sesnCnt,
+        logPath: this.procInfo.logPath,
+        logFileNm: this.procInfo.logFileNm,
+        socketTimeout: this.procInfo.socketTimeout,
+        mqTimeout: this.procInfo.mqTimeout,
+        pgmTyp: this.procInfo.pgmTyp,
+        linkTyp: this.procInfo.linkTyp,
+        dvpSvrNum: this.procInfo.dvpSvrNum,
+        dvpSvrPort: this.procInfo.dvpSvrPort,
+        prodSvrNum: this.procInfo.prodSvrNum,
+        prodSvrPort: this.procInfo.prodSvrPort,
+      })
+        .then((res) => {
+          console.log(res);
+          if (res.data.rstCd === 'S') {
+            this.initProcInfo();
+            this.searchProcList(this.onlineInfo.onlineMetaNum);
+            this.$gf.alertOn('프로세스 정보가 변경되었습니다.');
+          } else {
+            this.$gf.alertOn('failed');
+          }
+        })
+        .catch((ex) => {
+          console.log(`error occur!! : ${ex}`);
+        });
+    },
+    deleteProcInfo(procNum, index) {
+      eigwApi.fetchDeleteEigwProcInfo({
+        params: {
+          procNum,
+          onlineMetaNum: this.onlineInfo.onlineMetaNum,
+        },
+      })
+        .then((res) => {
+          console.log(res);
+          if (res.data.rstCd === 'S') {
+            this.$gf.alertOn('프로세스가 삭제되었습니다.');
+            this.procList.splice(index, 1);
+            this.initProcInfo();
+          } else {
+            this.$gf.alertOn('failed');
+          }
+        })
+        .catch((ex) => {
+          console.log(`error occur!! : ${ex}`);
+        });
+    },
+    turOffSvrPop(val) {
+      console.log(val);
+      this.svrOn = false;
+    },
+    setSvr(val) {
+      if (this.serverPopupCase === 1) {
+        this.procInfo.dvpSvrNum = val.svrNum;
+        this.procInfo.dvpSvrRealIp = val.svrRealIp;
+        this.procInfo.dvpSvrNatIp = val.svrNatIp;
+      } else {
+        this.procInfo.prodSvrNum = val.svrNum;
+        this.procInfo.prodSvrRealIp = val.svrRealIp;
+        this.procInfo.prodSvrNatIp = val.svrNatIp;
+      }
+      this.svrOn = false;
+    },
+    turnOnSvrPop(val) {
+      this.serverPopupCase = val;
+      this.svrOn = true;
+    },
+    selectProcInfo(i) {
+      this.procInfo = this.procList[i];
     },
   },
 };
