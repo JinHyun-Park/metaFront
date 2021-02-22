552ccd6ae0a712cdaf9aa01162b3090929eb1d86
P064066 <P064066@SOCVDI-C0071.SKT.AD>
2021-02-22 15:01:39 +0900

    개요 2번 내용 수정

--- src/views/guide/ProcGuideStep2.vue
+++ src/views/guide/ProcGuideStep2.vue
@@ -3,169 +3,75 @@
     <section class="title style-1">
       <h2>
         <div>
-          <i class="ico-bar" />개발 시 진행 프로세스(신규 연동 시스템 기준)
+          <i class="ico-bar" />개발 시 진행 프로세스(신규/추가 연동 시스템 기준)
         </div>
       </h2>
     </section>
     <section>
-      <div>
-        <span class="subtitle1">
-          1. EAI
-        </span>
-      </div>
-    </section>
-    <section class="form_area">
-      <div class="table_grid">
+      <div class="table_grid tb_layout">
         <div class="table_head w-auto">
           <ul>
-            <li class="th_cell">
+            <li
+              class="th_cell"
+              style="width:5%;"
+            >
               순번
             </li>
-            <li class="th_cell">
-              진행내용
+            <li
+              class="th_cell"
+              style="width:15%;"
+            >
+              단계
             </li>
-            <li class="th_cell">
+            <li
+              class="th_cell"
+              style="width:25%;"
+            >
+              EAI
+            </li>
+            <li
+              class="th_cell"
+              style="width:24%;"
+            >
+              EIGW
+            </li>
+            <li
+              class="th_cell"
+              style="width:20%;"
+            >
+              MCG
+            </li>
+            <li
+              class="th_cell"
+              style="width:8%;"
+            >
               담당자
             </li>
-            <li class="th_cell">
+            <li
+              class="th_cell"
+              style="width:4%;"
+            >
               Check
             </li>
           </ul>
         </div>
         <div class="table_body">
-          <ul class="table_row w-auto">
-            <li class="td_cell">
+          <ul class="table_row">
+            <li class="td_cell" >
               1
+            </li>
+            <li class="td_cell">
+              사전작업 완료 확인
             </li>
             <li class="td_cell">
               신규 연동 시스템과 EAI HUB 간 방화벽 Open
             </li>
             <li class="td_cell">
-              신청자
+              신규 연동 기관 서버 정보 EIGW담당자와 공유<br/>
+              (방화벽 신청은 EIGW담당자가 진행)
             </li>
             <li class="td_cell">
-              <input type="checkbox">
-            </li>
-          </ul>
-          <ul class="table_row w-auto">
-            <li class="td_cell">
-              2
-            </li>
-            <li class="td_cell">
-              EAI 파일 시스템 구성(10GB)
-            </li>
-            <li class="td_cell">
-              신청자
-            </li>
-            <li class="td_cell">
-              <input type="checkbox">
-            </li>
-          </ul>
-          <ul class="table_row w-auto">
-            <li class="td_cell">
-              3
-            </li>
-            <li class="td_cell">
-              EAI 환경 구성 및 관리를 위한 OS 계정(eaiadmin) 생성
-            </li>
-            <li class="td_cell">
-              신청자
-            </li>
-            <li class="td_cell">
-              <input type="checkbox">
-            </li>
-          </ul>
-          <ul class="table_row w-auto">
-            <li class="td_cell">
-              4
-            </li>
-            <li class="td_cell">
-              EAI 사용을 위한 eaiadmin 및 application 서비스 계정 권한 설정
-            </li>
-            <li class="td_cell">
-              신청자
-            </li>
-            <li class="td_cell">
-              <input type="checkbox">
-            </li>
-          </ul>
-          <ul class="table_row w-auto">
-            <li class="td_cell">
-              5
-            </li>
-            <li class="td_cell">
-              신규 연동 시스템 서버 접근 권한 신청
-            </li>
-            <li class="td_cell">
-              EAI 담당자 or 신청자
-            </li>
-            <li class="td_cell">
-              <input type="checkbox">
-            </li>
-          </ul>
-          <ul class="table_row w-auto">
-            <li class="td_cell">
-              6
-            </li>
-            <li class="td_cell">
-              EAI Component 설치 및 인터페이스 환경 구성
-            </li>
-            <li class="td_cell">
-              EAI 담당자
-            </li>
-            <li class="td_cell">
-              <input type="checkbox">
-            </li>
-          </ul>
-          <ul class="table_row w-auto">
-            <li class="td_cell">
-              7
-            </li>
-            <li class="td_cell">
-              프로그램 개발 및 테스트
-            </li>
-            <li class="td_cell">
-              신청자
-            </li>
-            <li class="td_cell">
-              <input type="checkbox">
-            </li>
-          </ul>
-        </div>
-      </div>
-    </section>
-    <section>
-      <div>
-        <span class="subtitle1">
-          2. MCG
-        </span>
-      </div>
-    </section>
-    <section class="form_area">
-      <div class="table_grid">
-        <div class="table_head w-auto">
-          <ul>
-            <li class="th_cell">
-              순번
-            </li>
-            <li class="th_cell">
-              진행내용
-            </li>
-            <li class="th_cell">
-              담당자
-            </li>
-            <li class="th_cell">
-              Check
-            </li>
-          </ul>
-        </div>
-        <div class="table_body">
-          <ul class="table_row w-auto">
-            <li class="td_cell">
-              1
-            </li>
-            <li class="td_cell">
-              신규 연동 채널과 MCG 간 방화벽 Open
+              신규 채널과 MCG 간 방화벽 Open
             </li>
             <li class="td_cell">
               신청자
@@ -179,7 +85,20 @@
               2
             </li>
             <li class="td_cell">
-              Inbound 채널의 경우 가상사용자 신청
+              연동 설정 작업 요청/확인
+            </li>
+            <li class="td_cell">
+              . EAI 파일 시스템 구성 (10GB)<br/>
+              . EAI 환경을 위한 OS 계정(eaiadmin) 생성<br/>
+              . eaiadmin 및 application 서비스 계정 권한 설정<br/>
+              . 신규 연동 시스템 서버 접근 권한 신청<br/>
+              . EAI Component 설치 및 인터페이스 환경 구성
+            </li>
+            <li class="td_cell">
+              관련 EAI 인터페이스 구성 확인
+            </li>
+            <li class="td_cell">
+              Inbound 채널의 경우, 가상신청자 신청
             </li>
             <li class="td_cell">
               신청자
@@ -191,12 +110,21 @@
           <ul class="table_row w-auto">
             <li class="td_cell">
               3
+            </li>
+            <li class="td_cell">
+              신청 인터페이스 구성
+            </li>
+            <li class="td_cell">
+              EAI 신청 인터페이스 구성
+            </li>
+            <li class="td_cell">
+              EIGW 연동 모듈 개발
             </li>
             <li class="td_cell">
               신규 채널 및 거래 등록
             </li>
             <li class="td_cell">
-              MCG 담당자
+              인터페이스 담당자
             </li>
             <li class="td_cell">
               <input type="checkbox">
@@ -209,6 +137,13 @@
             <li class="td_cell">
               프로그램 개발 및 테스트
             </li>
+            <li class="td_cell">
+              
+            </li>
+            <li class="td_cell">
+              
+            </li>
+            <li class="td_cell"></li>
             <li class="td_cell">
               신청자
             </li>
