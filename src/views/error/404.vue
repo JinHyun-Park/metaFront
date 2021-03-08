1e08c3a72bcf6445f985733049df3de91a93a1c8
P086160 <skt.P086160@partner.sk.com>
2021-03-08 17:41:17 +0900

    404 페이지 수정중

--- src/views/error/404.vue
+++ src/views/error/404.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="center_404">
-    <img
-      src="@/assets/images/notfound.jpg"
-      width="600"
-    >
-    <div>
-      <h1>올바르지 않은 접근입니다.</h1>
+  <div class="">
+    <div class="center_404">
+      <img
+        src="@/assets/images/notfound.png"
+        width="600"
+      >
+    </div>
+    <div class="contents_text">
+      <span>올바르지 않은 접근입니다.</span>
     </div>
   </div>
 </template>
@@ -19,12 +21,22 @@ export default {
 </script>
 
 <style scoped>
-.center_404 {
+.contents_404 {
   width: 100%;
+}
+.center_404 {
+  width: 50%;
   position: absolute;
   left: 50%;
   margin-left: -300px;
   top: 20%;
+}
+.contents_text {
+  position: absolute;
+  width: 100%;
+  text-align: center;
+  top: 70%;
+  font-size: 30px;
 }
 
 </style>
